923e4d9d89fe67dee3829905a195c638c813deed
aliceborges <aliceborges@outlook.com.br>
2019-03-21 01:15:40 -0300

    criado um js para manipular a senha de entrada no site

---
+++ casamento/js/password.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('body').css({'backgroundImage': "url('images/lock.jpg')"});
+
+  function askPassword(again = false){
+    content = again ? 'A senha informada está errada. \n\n Qual é a senha?' : 'Qual é a senha?';
+    let password = prompt(content);
+    return password;
+  }
+
+  function doAsk(again = false){
+    password = askPassword(again);
+    const sitePassword = '070719aem';
+
+    if (password == sitePassword){
+      $('#locked').hide();
+      $('body').css({'backgroundImage': "url('images/dust_scratches.png')"});
+      $('#free').show();
+    }else{
+      doAsk(true);
+    }
+  }
+
+  setTimeout(function(){
+    doAsk();
+  }, 600);
+});
